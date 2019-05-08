32c18f1fb5a69d502135ea7e5d938347c79ecb04
Rodrigo Costa <costa.rgc@gmail.com>
2019-05-08 14:43:08 -0400

    extract working

--- bmf_scraper.js
+++ bmf_scraper.js
@@ -1,38 +1,47 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
-const url = 'http://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp?Data='
+const searchbmf = async (Data, slcTaxa) => {
+    const url = 'http://www2.bmf.com.br/pages/portal/bmfbovespa/boletim1/TxRef1.asp?Data=' + Data + "&slcTaxa=" + slcTaxa;
+    const response = await fetch(url);
+    const body = await response.text();
+    const $ = cheerio.load(body);
 
-function searchbmf(Data,slcTaxa){
-    Data=Data;
-    slcTaxa=slcTaxa;
-    return fetch(url+Data+"&slcTaxa="+slcTaxa).then(response => response.text());
+    linebreak = $('#tb_principal1').find('tr').replaceWith('\t');
+    var arr = [];
+    linebreak.each(function (i, element) {
+        const $element = $(element);
+        const $data = $element.find('td').text().replace(/,/g, '.').replace('\n', '\t').replace(' \n', '\t');
+        arr[i] = $data;
+    })
+
+    console.log(arr.length);
+    console.log(arr[arr.length-1]);
 }
 
-//search receide 
-//date >> DD/MM/YYYY
-//slcTaxa >> <AAA>
-
-searchbmf('08/06/2018','PRE').then(body => {
-    const mydata = [];
-    var $ = cheerio.load(body);
+searchbmf('08/06/2018', 'PRE');
 
 
-    $('table.tabConteudo').each(function(i, element){
-        const $element = $(element);
-        const $rate1 = $element.find('td.tabelaConteudo1').text();
-       
-        console.log($rate1);
-
-        // const mydata = {
-        //     date: $fields[0].text(),
-
-        // };
-        // mydata.push(mydata);
-
-      })
-        
-    //   console.log(mydata);
-      //console.log(fields);
-
-})
+// | slcTaxa  | name |
+// | ------------- | ------------- |
+// | ACC  | Ajuste cupom  |
+// | APR  | Ajuste pré  |
+// | BRP| IBrX-50 |
+// | DCO| Cupom Cambial OC1 |
+// | DIC| DI x IPCA |
+// | DIM| DI x IGP-M |
+// | DOC| Cupom limpo |
+// | DOL| DI x dólar |
+// | DP| Dólar x pré |
+// | EUC| DI x euro |
+// | EUR| Real x euro |
+// | INP| Ibovespa |
+// | JPY| Real x iene |
+// | LIB| Libor |
+// | PRE| DI x pré |
+// | PTX| Real x dólar |
+// | SDE| Spread Libor Euro x Dólar |
+// | SLP| Selic x pré |
+// | TFP| TBF x pré |
+// | TP| TR x pré |
+// | TR| DI x TR |
